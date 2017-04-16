496dba1ed73061ac316806d50825a53d25d44f39
g1v <hj.923.ku@gmail.com>
2017-04-17 02:29:22 +1000

    Update me_it.js

--- me_it.js
+++ me_it.js
@@ -1,3 +1,11 @@
 
-pq = {};
-pq.kek = 'lol';
+pq = {
+  point: {
+    get: function() {
+        return parseInt($('.nav__points').html());
+    },
+    set: function(a) {
+        $('.nav__points').html(a);
+    }
+  }
+};
