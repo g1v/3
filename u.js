b9080ed4e2d74abd795116f1ac18830b6174aa21
g1v <hj.923.ku@gmail.com>
2017-04-21 06:42:39 +1000

    Create u.js

---
+++ u.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Auto gift farming
+// @version      0-0-2-7
+// @author       Sir TE5T
+// @updateURL    https://raw.githubusercontent.com/g1v/3/master/v.js
+// @downloadURL  https://raw.githubusercontent.com/g1v/3/master/u.js
+// @require      http://code.jquery.com/jquery-3.2.1.min.js
+// @require      https://raw.githubusercontent.com/g1v/3/master/pq.js
+// @require      https://raw.githubusercontent.com/g1v/3/master/m.js
+// @match        www.steamgifts.com/*
+// ==/UserScript==
+
+$(document).ready(startup);
