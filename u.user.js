c7c847bc024a933e4a93cccd8c56e79e4f0e8436
g1v <hj.923.ku@gmail.com>
2017-04-21 06:57:06 +1000

    Update and rename u.js to u.user.js

--- u.user.js
+++ u.user.js
@@ -2,13 +2,13 @@
 // @name         Auto gift farming
 // @version      0-0-3-0
 // @author       Sir TE5T
-// @updateURL    https://raw.githubusercontent.com/g1v/3/master/v.js
-// @downloadURL  https://raw.githubusercontent.com/g1v/3/master/u.js
+// @updateURL    https://raw.githubusercontent.com/g1v/3/master/v.meta.js
+// @downloadURL  https://raw.githubusercontent.com/g1v/3/master/u.user.js
 // @require      http://code.jquery.com/jquery-3.2.1.min.js
 // @require      https://raw.githubusercontent.com/g1v/3/master/pq.js
 // @require      https://raw.githubusercontent.com/g1v/3/master/m.js
 // @match        www.steamgifts.com/*
 // ==/UserScript==
 
-// ver test -- 1
+// aaaaa
 $(document).ready(startup);
