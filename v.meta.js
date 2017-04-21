d2ba53e05b0144fe42115116c25679d4abb84fe6
g1v <hj.923.ku@gmail.com>
2017-04-21 19:38:39 +1000

    Update v.meta.js

--- v.meta.js
+++ v.meta.js
@@ -1,3 +1,9 @@
 // ==UserScript==
-// @version      3.0
+// @name         Auto gift farming
+// @version      3.1
+// @author       Sir TE5T
+// @updateURL    https://raw.githubusercontent.com/g1v/3/master/v.meta.js
+// @downloadURL  https://raw.githubusercontent.com/g1v/3/master/u.user.js
+// @require      http://code.jquery.com/jquery-3.2.1.min.js
+// @match        www.steamgifts.com/*
 // ==/UserScript==
